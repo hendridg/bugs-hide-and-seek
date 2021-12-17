d3d4a6b0d0b9beda98a7fcc49d4e2bcccece187c
hendridg <hendridg@gmail.com>
2021-12-17 18:17:49 -0400

    Add index.js with event in button

---
+++ index.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('.form-example');
+const [name, email, btn] = document.querySelectorAll('form-example');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+});
